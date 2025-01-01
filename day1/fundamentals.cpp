a2d47cdcfd73b671c6c00a2c24f35085dc60a8d8
sagar reddy <sagarreddy@sagars-MacBook-Air.local>
2025-01-01 14:35:54 +0530

    first program running fine

--- day1/fundamentals.cpp
+++ day1/fundamentals.cpp
@@ -1,9 +1,21 @@
-#include <stdio.h>
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "Hello C++!" << endl;
-    cout << "Hello World!" << endl;
+    int num;
+    int howMany;
+    cout << "Enter how many numbers you want to add: ";
+    cin >> howMany;
+
+    int sum = 0;
+
+    for (int i = 0; i < howMany; i++)
+    {
+        cout << "Enter your number: ";
+        cin >> num;
+        sum += num;
+    }
+    cout << "The sum of the numbers is " << sum;
+    return 0;
 }
